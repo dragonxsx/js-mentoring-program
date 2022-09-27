d2b9090cae0ba751b04437ee1b0898199d2f6335
Long Nguyen <long_nguyen@epam.com>
2022-09-27 09:32:55 +0700

    Module 01 - Task 1

---
+++ 01-nodejs-fundamental/homework/task-01.js
@@ -0,0 +1,23 @@
+// SOLUTION #1
+import { Transform } from "stream"
+
+const reverse = new Transform({
+    transform(chunk, endcoding, callback) {
+        const reversedString = chunk.toString().split("").reverse().join("")
+        callback(null, `${reversedString}\n`)
+    }
+})
+
+process.stdin.pipe(reverse).pipe(process.stdout)
+
+// SOLUTION #2
+// import readline from "readline"
+
+// const rl = readline.createInterface({
+//     input: process.stdin
+// })
+
+// rl.on("line", (line) => {
+//     const reversedString = line.split("").reverse().join("")
+//     process.stdout.write(reversedString + "\n")
+// })
